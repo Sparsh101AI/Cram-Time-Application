7489e3eb01a702aa89a924d7b78958643480bffb
Darsh <darshpatelcr7@gmail.com>
2022-01-02 18:16:46 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import FirstPage from "./FirstPage/FirstPage";
 import Navbar from "./Navbar/Navbar";
@@ -10,10 +10,10 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/FirstPage" exact component={<FirstPage />} />
-        </Routes>
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/FirstPage" exact component={FirstPage} />
+        </Switch>
       </div>
     </Router>
   );
