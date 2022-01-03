489af226659393dc38c3fb86c7b6b6a8be026317
Darsh <darshpatelcr7@gmail.com>
2022-01-02 21:02:39 -0700

    added particles

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,9 +1,101 @@
 import "./HomePage.css";
 import image1 from "../images/image1.png";
+import Particles from "react-tsparticles";
 
 function HomePage() {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div className="container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#a0abe3",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: false,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#b2bdfb",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: false,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: false,
+              value: 25,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <div className="box">
         <div className="col1">
           <img src={image1} alt="idk" />
@@ -17,7 +109,6 @@ function HomePage() {
           <button className="btn">Join Now</button>
         </div>
       </div>
-      <div className="Circle"></div>
     </div>
   );
 }
