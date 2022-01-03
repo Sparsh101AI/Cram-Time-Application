46d8b5215feed67b618c82bc4f0b9e0503d6ef32
Darsh <darshpatelcr7@gmail.com>
2022-01-02 17:58:54 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import FirstPage from "./FirstPage/FirstPage";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/FirstPage" exact component={<FirstPage />} />
