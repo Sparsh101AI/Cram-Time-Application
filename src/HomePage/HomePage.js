d9a2e065d9b0388a744a3dd3907b5403513e1191
Darsh <darshpatelcr7@gmail.com>
2022-01-03 22:10:07 -0700

    features addition

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,7 +1,8 @@
 import "./HomePage.css";
 import image1 from "../images/image1.png";
+import image2 from "../images/image2.png";
 import Particles from "react-tsparticles";
-import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 
 function HomePage() {
   const particlesInit = (main) => {
@@ -102,31 +103,45 @@ function HomePage() {
           <img src={image1} alt="idk" />
         </div>
         <div className="col2">
-       
-        <h1 >
-        Why Study Alone, When You <br /> Can Study With{" "}
-       
-        <span>
-          {/* Style will be inherited from the parent element */}
-         <strong>
-          <Typewriter
-            words={['Friends', 'Peers', 'Classmates']}
-            loop
-            cursor
-            cursorStyle='_'
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-             />
-          </strong>
-        </span>
-       
-      </h1>
+          <h1>
+            Why Study Alone, When You <br /> Can Study With{" "}
+            <span>
+              {/* Style will be inherited from the parent element */}
+              <strong>
+                <Typewriter
+                  words={["Friends", "Peers", "Classmates"]}
+                  loop
+                  cursor
+                  cursorStyle="_"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </strong>
+            </span>
+          </h1>
 
           <h2>Get started now and start studying together</h2>
           <button className="btn">Join Now</button>
         </div>
       </div>
+      <hr className="line" />
+      <div className="Feature">
+        <div className="col3">
+          <img src={image2} alt="idk" />
+        </div>
+        <div className="col4">
+          <h1 className="header">
+            Study Using <strong>Flash Cards</strong>
+          </h1>
+          <h2>
+            Make your study sessions interesting using Flash Cards! Flash Cards
+            are an effective method to memorize your study material and you can
+            have fun testing yourself amongst your friends using CramTime
+          </h2>
+        </div>
+      </div>
+      <hr className="line" />
     </div>
   );
 }
